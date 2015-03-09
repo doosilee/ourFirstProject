03880532d79a5828aae76b2b9479477cbce5567b
LeeDoona(이두나) <nesk93@naver.com>
2015-03-09 22:02:51 +0900

    얼굴유사도 - 파일불러오기 실패

--- firstProject/firstProject/utils7.hpp
+++ firstProject/firstProject/utils7.hpp
@@ -165,3 +165,123 @@ Mat detect_object(Mat srcImg, Rect *rects, Point *points, CascadeClassifier &fac
 	}
 	return Mat();
 }
+
+const char* facerecAlgorithm = "FaceRecognizer.Fisherfaces";		//LDA얼굴인식
+const double EYE_LEFT = 0.16;
+const double EYE_RIGHT = (1.0f - EYE_LEFT);
+const double EYE_LEFT_Y = 0.14;
+
+
+//---------------------------------------------------------------------------------------
+//영상 읽기 = 예외처리 포함
+Mat readImage(char* fn, int mode = 1)
+{
+	string image_path = "../../image/";			//이미지 파일 기본 경로
+	Mat image = imread(image_path + fn, mode);
+	if(image.empty()) {
+		cout<< "파일이 없습니다." <<endl;
+		exit(1);
+	}
+	return image;
+}
+
+//평균얼굴과 고유벡터로 얼굴 재구성
+Mat reconstructFace(const Ptr<FaceRecognizer> model, const Mat preprocessedFace)
+{
+	try{
+		//고유 벡터와 평균 얼굴
+		Mat eigenvectors = model->get<Mat>("eigenvectors");
+		Mat mean_face    = model->get<Mat>("mean");				//1행 데이터로 가져옴
+
+		//-------------------------------------
+		//1행 데이터 구성 및 프로젝션 수행
+		Mat oneRow_face = preprocessedFace.reshape(1, preprocessedFace.rows);
+		Mat projection = subspaceReconstruct(eigenvectors, mean_face, projection);
+		Mat reconstructionRow = subspaceReconstruct(eigenvectors, mean_face, projection);
+
+		//-------------------------------------
+		//원 얼굴 검출 영상 크기로 재구성
+		Mat reconstruc_face = reconstructionRow.reshape(1, preprocessedFace.rows);
+		reconstruc_face.convertTo(reconstruc_face, CV_8U);
+
+		return reconstruc_face;
+	}catch(cv::Exception e) {
+		return Mat();
+	}
+}
+
+//Compare two images by getting the L2 error(square-root of sum of squared error).
+double getSimilarity(const Mat A, const Mat B)
+{
+	if(A.total()>0 && A.total() == B.total()) {
+		return norm(A, B, NORM_L2)/(A.total());			//두 영상이 비슷할수록 작은 값
+	}
+	else {
+		return 100000000.0;								//에러 값 반환
+	}
+}
+
+//얼굴 기울기 보정 및 타원 배경 추가
+Mat rotated_face(Mat faceImg, Point* eyes, Size n_face)
+{
+	//-----------------------------------
+	//회전 보정 및 배경에 마스크 설정
+	if(eyes[1].x >= 0 && eyes[2].x >= 0)
+	{
+		//------------------------------
+		//두 눈 거리 및 보정 비율
+		Point2f delta = eyes[2] - eyes[1];
+		double angle = atan2(delta.y, delta.x) * 180.0/CV_PI;	//두 눈의 기울기 계산
+		double eyes_len = n_face.width * (EYE_RIGHT - EYE_LEFT);//보정길이
+		double scale = eyes_len / sqrt(delta.x*delta.x + delta.y*delta.y);
+
+		//------------------------------
+		//회전각 계산
+		int n_face_size_2 = n_face.width/2;						//얼굴 가로 중심
+		Point2f eyesCenter = eyes[2] + eyes[1];					//눈 중심 좌표
+		eyesCenter.x /= 2, eyesCenter.y /= 2;
+		Mat rot_mat = getRotationMatrix2D(eyesCenter, angle, scale);//회전 보정 행렬
+		rot_mat.at<double>(0, 2) += n_face_size_2 - eyesCenter.x;   //x축 평행이동
+		rot_mat.at<double>(1, 2) += n_face.width * EYE_LEFT_Y - eyesCenter.y;//y축 평행이동
+
+		//------------------------------
+		//회전 보정 수행 - 기울어진 얼굴 보정
+		Mat warped = Mat(n_face, CV_8U, Scalar(128));
+		warpAffine(faceImg, warped, rot_mat, warped.size());
+
+		equalizeHist(warped, warped);					//히스토그램 평활화
+		Mat mask = Mat(n_face, CV_8U, Scalar(0));		//회색배경 마스크
+		Mat dstImg = Mat(n_face, CV_8U, Scalar(128));	//최종 반환 얼굴 영상
+
+		//-----------------------------
+		//배경영역에 타원 추가
+		Point faceCenter(n_face_size_2, cvRound(n_face.width * 0.4f));//타원 중심
+		Size size(n_face_size_2, cvRound(n_face.width * 0.8f));			//타원 크기
+		ellipse(mask, faceCenter, size, 0, 0, 360, Scalar(255), CV_FILLED);//마스크 중심부 흰색 설정
+		warped.copyTo(dstImg, mask);
+		return dstImg;
+	}
+	else 
+		return Mat();
+}
+
+
+void display_topface(Mat &frame, Mat corrected_face, Size n_face, int BORDER)
+{
+
+	//--------------------------------
+	//검출된 얼굴 컬러 변환
+	Mat tmpBGR;
+	cvtColor(corrected_face, tmpBGR, CV_GRAY2BGR);	//검출된 얼굴 컬러 영상으로 표시
+
+	//--------------------------------
+	//프레임 중심 위치 참조
+	int centerx = (frame.cols - n_face.width) /2;
+	Rect face_roi(Point(centerx, BORDER), n_face);	//상단 중앙 사각형
+	Mat topface = frame(face_roi);					//사각형 만큼 frame참조
+
+	//---------------------------------
+	//컬러 검출된 얼굴 복사 및 사각형 그리기
+	tmpBGR.copyTo(topface);
+	rectangle(frame, face_roi-Point(1, 1)+Size(2, 2), CV_RGB(200, 200, 200), 1, CV_AA);
+}
